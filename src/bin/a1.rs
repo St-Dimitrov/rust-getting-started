e75afd38c70edbb354606d4c86cec2aaba1e94a3
Stefan Dimitrov <stefan.r7.dimitrov@gmail.com>
2021-12-07 15:35:09 +0200

    added solution for exercise

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,16 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+fn first_name(){
+    println!("Stefan")
+}
+
+fn last_name() {
+    println!("Dimitrov")
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
