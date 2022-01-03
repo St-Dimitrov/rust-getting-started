4cd9127dd48dd2824b2f1c893adc836dedd63723
Stefan Dimitrov <stefan.r7.dimitrov@gmail.com>
2022-01-03 23:52:56 +0200

    adding expression example

---
+++ src/bin/a10.rs
@@ -0,0 +1,25 @@
+// Topic: Working with expressions
+//
+// Requirements:
+// * Print "its big" if a variable is > 100
+// * Print "its small" if a variable is <= 100
+//
+// Notes:
+// * Use a boolean variable set to the result of
+//   an if..else expression to store whether the value
+//   is > 100 or <= 100
+// * Use a function to print the messages
+// * Use a match expression to determine which message
+//   to print
+fn print_message(a: bool) {
+    match a {
+        true => println!("it's big"),
+        false => println!("it's small")
+    }
+}
+
+fn main() {
+    let my_num = 11;
+    let my_bool = my_num > 100;
+    print_message(my_bool);
+}
